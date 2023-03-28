61b22f38b37fdf19fb72b9bc958b599c00f81dbc
rajat1895 <rajat1895@gmail.com>
2023-03-28 10:44:29 -0400

    Created Q/A chat bot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Configuration, OpenAIApi } from 'openai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import FormSection from './Components/FormSection';
+import AnswerSection from './Components/AnswerSection';
+import { useState } from 'react';
+
+const App = () => {
+    const configuration = new Configuration({
+        apiKey: "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",
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const [storedValues, setStoredValues] = useState([]);
+
+    const generateResponse = async (newQuestion, setNewQuestion) => {
+        let options = {
+            model: 'text-davinci-003',
+            temperature: 0,
+            max_tokens: 100,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ['/'],
+        };
+
+        let completeOptions = {
+            ...options,
+            prompt: newQuestion,
+        };
+
+        const response = await openai.createCompletion(completeOptions);
+        console.log(response.data.choices[0].text);
+
+        if (response.data.choices) {
+            setStoredValues([
+                {
+                    question: newQuestion,
+                    answer: response.data.choices[0].text,
+                },
+                ...storedValues,
+            ]);
+            setNewQuestion('');
+        }
+    };
+
+    return (
+        <div>
+            <div className="header-section">
+                <h1>ChatGPT CLONE 🤖</h1>
+                    <p>
+                        Hi I am your Virtual Assistant to answer your Questions. I am based on OpenAI's ChatGPT-4 Api.
+                    </p>
+            </div>
+
+            <FormSection generateResponse={generateResponse} />
+
+            <AnswerSection storedValues={storedValues} />
+        </div>
+    );
+};
 
 export default App;
